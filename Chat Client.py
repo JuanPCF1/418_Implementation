db5fb6514bb6c46eb2b1b005fd628550982057ae
JuanPCF1 <107865734+JuanPCF1@users.noreply.github.com>
2025-03-30 20:33:37 -0600

    Some work on the algo
    - Created a double ratchet class that generates the shared keys and derives the root, chain send, and chain receive key.

--- Chat Client.py
+++ Chat Client.py
@@ -12,12 +12,51 @@ from cryptography.hazmat.primitives.asymmetric.utils import encode_dss_signature
 from cryptography.hazmat.backends import default_backend
 from base64 import b64encode, b64decode
 
+class DoubleRatchet:
+    def __init__(self):
+        # Set up the diffie-hellman parameters using the cryptography library
+        self.parameters = dh.generate_parameters(generator=2, key_size=512, backend=default_backend())
+        
+        # Generate a private key and private key pair
+        self.private_key = self.parameters.generate_private_key()
+        self.public_key = self.private_key.public_key()
+        
+        # Variables to keep track of the state of the ratchet
+        self.root_key = None
+        self.chain_key_send = None
+        self.chain_key_receive = None
+        self.message_number_send = 0
+        self.message_number_receive = 0
+        
+        # Storest the public key we will get from the server
+        self.server_public_key = None
+    
+    def generate_shared_key(self, server_public_key):
+        # Generate a shared key using the server's public key and our private key
+        shared_key = self.private_key.exchange(server_public_key)
+        
+    def derive_keys(self, shared_key):
+        # Derive the root key from the shared key using HKDF
+        self.derived_key = HKDF(
+            algorithm=hashes.SHA256(),
+            length=64,
+            salt=None,
+            info=b'Diffie-Hellman key derivation using HKDF',
+            backend=default_backend()
+        ).derive(shared_key)
+        
+        # Use the derived key to create the root key and chain keys
+        self.root_key = self.derived_key[:32]
+        self.chain_key_send = self.derived_key[32:48]
+        self.chain_key_receive = self.derived_key[48:]
+        
+
 # Parameters for the diffie-hellman key exchange
-# parameters = dh.generate_parameters(generator=2, key_size=512, backend=default_backend())
+parameters = dh.generate_parameters(generator=2, key_size=512, backend=default_backend())
 
 
-#def encrypt_message(message, key):
-#    init_vector = os.urandom(16)
+def encrypt_message(message, key):
+    init_vector = os.urandom(16)
 
 def receive_messages(client_socket,):
     while True:
